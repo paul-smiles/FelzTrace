528bc6b2625a2b5cd2113b0d7eb27d3da9ff5d05
Paul Krickeberg <paul@krickeberg.de>
2026-01-05 01:34:12 +0100

    Add: cppcheck analysis tool
    * configure Cmake for cppcheck and adjust for clang-tidy
    * add cppcheck to docker dev image
    * add CI for cppcheck and clang-tid
    (ref #3)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -e
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build --config Release -- -j$(nproc)
