35bda0a30db5b72599a4950ec6a9eed437dc2267
Paul Krickeberg <paul@krickeberg.de>
2026-01-09 23:35:56 +0100

    Change: proposals from review
    (ref #3)

--- scripts/check-clang-format.sh
+++ scripts/check-clang-format.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Optional: sicherstellen, dass wir wirklich in einem Git-Repo sind
+# Optional: assure we are inside a git repository
 git rev-parse --is-inside-work-tree >/dev/null
 
-# Dateien einsammeln und clang-format prüfen
+# Collect files and check clang-format
 git ls-files -z '*.cpp' '*.h' '*.hpp' \
   | xargs -0 -r clang-format --dry-run -Werror
